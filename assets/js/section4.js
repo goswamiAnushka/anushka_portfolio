ad39276f6eb7299566e180d1fd78518e23bec71b
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-21 03:29:26 +0530

    changed index image

--- assets/js/section4.js
+++ assets/js/section4.js
@@ -32,19 +32,19 @@ let progress = document.getElementById('progressbar');
             let flowBar1 = document.getElementById('fe-bar');
             let flowBar2 = document.getElementById('be-bar');
             let flowBar3 = document.getElementById('cp-bar');
-            let flowBar4 = document.getElementById('uiux-bar');
+
             if (progressHeight > 3) {
 
                 flowBar1.style.animationName = 'animateskillbar11';
                 flowBar2.style.animationName = 'animateskillbar22';
                 flowBar3.style.animationName = 'animateskillbar33';
-                flowBar4.style.animationName = 'animateskillbar44';
+               
             }
             else {
                 flowBar1.style.animationName = 'animateskillbar1';
                 flowBar2.style.animationName = 'animateskillbar2';
                 flowBar3.style.animationName = 'animateskillbar3';
-                flowBar4.style.animationName = 'animateskillbar4';
+              
             }
 
     }
